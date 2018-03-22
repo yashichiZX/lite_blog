bd2e55d7f3017a704c053f6954272ab3ba343b6c
itsumoonazicode <dyingkouki@gmail.com>
2018-03-23 01:22:25 +0900

    Google Analytics 設定追加

--- wp/wp-content/themes/twentyseventeen/header.php
+++ wp/wp-content/themes/twentyseventeen/header.php
@@ -15,6 +15,15 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116231297-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-116231297-1');
+</script>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
