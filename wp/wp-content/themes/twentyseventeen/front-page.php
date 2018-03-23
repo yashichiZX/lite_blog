8299abd12ea5acd2dd891bdcc12b96c965d991ed
itsumoonazicode <dyingkouki@gmail.com>
2018-03-24 00:41:23 +0900

    フロントページに吐き出すページコンテンツ数を一つに変更

--- wp/wp-content/themes/twentyseventeen/front-page.php
+++ wp/wp-content/themes/twentyseventeen/front-page.php
@@ -26,27 +26,6 @@ get_header(); ?>
 			get_template_part( 'template-parts/post/content', 'none' );
 		endif; ?>
 
-		<?php
-		// Get each of our panels and show the post data.
-		if ( 0 !== twentyseventeen_panel_count() || is_customize_preview() ) : // If we have pages to show.
-
-			/**
-			 * Filter number of front page sections in Twenty Seventeen.
-			 *
-			 * @since Twenty Seventeen 1.0
-			 *
-			 * @param int $num_sections Number of front page sections.
-			 */
-			$num_sections = apply_filters( 'twentyseventeen_front_page_sections', 4 );
-			global $twentyseventeencounter;
-
-			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-				$twentyseventeencounter = $i;
-				twentyseventeen_front_page_section( null, $i );
-			}
-
-	endif; // The if ( 0 !== twentyseventeen_panel_count() ) ends here. ?>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
